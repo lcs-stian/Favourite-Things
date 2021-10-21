34dd7da8a93438dd953b5b941248d9ed2f698190
Suzanne <STian@lcs.on.ca>
2021-10-21 14:56:34 -0400

    Initial Commit

---
+++ Favourite Things/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Favourite Things
+//
+//  Created by Suzanne Tian on 2021-10-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Favourite Things/Favourite_ThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Favourite_ThingsApp.swift
+//  Favourite Things
+//
+//  Created by Suzanne Tian on 2021-10-21.
+//
+
+import SwiftUI
+
+@main
+struct Favourite_ThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
