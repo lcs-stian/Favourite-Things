41884a24b5005e1d5c2d1ccddd49573726b88fec
Suzanne <STian@lcs.on.ca>
2021-11-09 10:43:24 -0500

    try to do list view (with errors)

--- Favourite Things/Views/FavouriteThingsListView.swift
+++ Favourite Things/Views/FavouriteThingsListView.swift
@@ -11,56 +11,86 @@ struct FavouriteThingsListView: View {
     var body: some View {
         List {
             
-            NavigationLink(destination: {
-                AngelView()
-            }, label: {
-                Text("Angel")
-            })
-            
-            
-            NavigationLink(destination: {
-                MaxView()
-            }, label: {
-                Text("Max")
-            })
-            
-            NavigationLink(destination: {
-                FOODView()
-            }, label: {
-                Text("FOOD")
-            })
-            
-            NavigationLink(destination: {
-                LittleRiceView()
-            }, label: {
-                Text("LittleRice")
-            })
-            
-            NavigationLink(destination: {
-                IcecreamView()
-            }, label: {
-                Text("Icecream")
-            })
-                           
-                           NavigationLink(destination: {
-                FogView()
-            }, label: {
-                Text("Fog")
-            })
-                           
-                           
-                           
-                           
-                           }
-                            .navigationTitle("Favourite Thing App")
-                           }
-                           }
-                           
-                           struct FavouriteThingsListView_Previews: PreviewProvider {
-                static var previews: some View {
-                    NavigationView {
-                        FavouriteThingsListView()
-                    }
-                    
-                }
+            NavigationLink(destination:AngelView())
+            HStack {
+                Image ("Angel")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 50, height: 50, alignment: .center)
+                    .clipped()
+                VStack {Text ("Angel")}
+                Text ("This is a huskey")
             }
+        
+        
+        NavigationLink(destination:MaxView())
+        HStack {
+            Image ("Max")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 50, height: 50, alignment: .center)
+                .clipped()
+            VStack {Text ("Max")}
+            Text ("This my dog")
+        }
+
+    
+    
+    
+    NavigationLink(destination:FOODView())
+    HStack {
+        Image ("FOOD")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 50, height: 50, alignment: .center)
+            .clipped()
+        VStack {Text ("FOOD")}
+        Text ("BBQ I had in the summer")
+    }
+
+
+NavigationLink(destination:LittleRiceView())
+HStack {
+    Image ("Icecream")
+        .resizable()
+        .scaledToFit()
+        .frame(width: 50, height: 50, alignment: .center)
+        .clipped()
+    VStack {Text ("Icecream")}
+    Text ("")
+}
+
+
+
+NavigationLink(destination:IcecreamView())
+HStack {
+    Image ("")
+        .resizable()
+        .scaledToFit()
+        .frame(width: 50, height: 50, alignment: .center)
+        .clipped()
+    VStack {Text ("")}
+    Text ("")
+}
+
+
+NavigationLink(destination:FogView())
+HStack {
+    Image ("")
+        .resizable()
+        .scaledToFit()
+        .frame(width: 50, height: 50, alignment: .center)
+        .clipped()
+    VStack {Text ("")}
+    Text ("")
+}}}
+        }
+            }
+
+.navigationTitle("Favourite Thing App")
+
+struct FavouriteThingsListView_Previews: PreviewProvider {
+    static var previews: some View {
+            FavouriteThingsListView()
+    }
+}
