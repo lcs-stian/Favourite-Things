ddc10fc5a6598445c40d7659187ca8e0a4aa89a9
Suzanne <STian@lcs.on.ca>
2021-11-09 11:40:35 -0500

    NavigationLink for six things

--- Favourite Things/Views/FavouriteThingsListView.swift
+++ Favourite Things/Views/FavouriteThingsListView.swift
@@ -11,86 +11,105 @@ struct FavouriteThingsListView: View {
     var body: some View {
         List {
             
-            NavigationLink(destination:AngelView())
-            HStack {
-                Image ("Angel")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 50, height: 50, alignment: .center)
-                    .clipped()
-                VStack {Text ("Angel")}
-                Text ("This is a huskey")
+            NavigationLink(destination: AngelView()) {
+                
+                HStack {
+                    Image ("Angel")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50, alignment: .center)
+                        .clipped()
+                    VStack {Text ("Angel")}
+                    Text ("This is a huskey")
+                }
+                
             }
-        
-        
-        NavigationLink(destination:MaxView())
-        HStack {
-            Image ("Max")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 50, height: 50, alignment: .center)
-                .clipped()
-            VStack {Text ("Max")}
-            Text ("This my dog")
+            
+            
+            NavigationLink(destination:MaxView()) {
+                
+                HStack {
+                    Image ("Max")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50, alignment: .center)
+                        .clipped()
+                    VStack {Text ("Max")}
+                    Text ("This my dog")
+                }
+                
+            }
+            
+            
+            
+            
+            
+            NavigationLink(destination:FOODView()) {
+                HStack {
+                    Image ("FOOD")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50, alignment: .center)
+                        .clipped()
+                    VStack {Text ("FOOD")}
+                    Text ("BBQ I had in the summer")
+                }
+                
+            }
+            
+            
+            NavigationLink(destination:LittleRiceView()) {
+                HStack {
+                    Image ("LittleRice")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50, alignment: .center)
+                        .clipped()
+                    VStack {Text ("LittleRice")}
+                    Text ("This a dog in had when I wI was little")
+                }
+            }
+            
+            
+            
+            NavigationLink(destination:IcecreamView()) {
+                HStack {
+                    Image ("Icecream")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50, alignment: .center)
+                        .clipped()
+                    VStack {Text ("Icecream")}
+                    Text ("I love Icecream")
+                }
+            }
+           
+            
+            
+            NavigationLink(destination:FogView()) {
+                HStack {
+                    Image ("Fog")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50, alignment: .center)
+                        .clipped()
+                    VStack {Text ("Fog")}
+                    Text ("These weather are the best")
+                }
+                
+            }
+           
         }
-
-    
-    
-    
-    NavigationLink(destination:FOODView())
-    HStack {
-        Image ("FOOD")
-            .resizable()
-            .scaledToFit()
-            .frame(width: 50, height: 50, alignment: .center)
-            .clipped()
-        VStack {Text ("FOOD")}
-        Text ("BBQ I had in the summer")
+        .navigationTitle("Favourite Thing App")
     }
-
-
-NavigationLink(destination:LittleRiceView())
-HStack {
-    Image ("Icecream")
-        .resizable()
-        .scaledToFit()
-        .frame(width: 50, height: 50, alignment: .center)
-        .clipped()
-    VStack {Text ("Icecream")}
-    Text ("")
 }
 
 
 
-NavigationLink(destination:IcecreamView())
-HStack {
-    Image ("")
-        .resizable()
-        .scaledToFit()
-        .frame(width: 50, height: 50, alignment: .center)
-        .clipped()
-    VStack {Text ("")}
-    Text ("")
-}
 
-
-NavigationLink(destination:FogView())
-HStack {
-    Image ("")
-        .resizable()
-        .scaledToFit()
-        .frame(width: 50, height: 50, alignment: .center)
-        .clipped()
-    VStack {Text ("")}
-    Text ("")
-}}}
-        }
-            }
-
-.navigationTitle("Favourite Thing App")
 
 struct FavouriteThingsListView_Previews: PreviewProvider {
     static var previews: some View {
-            FavouriteThingsListView()
+        FavouriteThingsListView()
     }
 }
